025ac034a7fecd0575dfda751b07ea867f154ace
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-26 01:55:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,23 +1,5 @@
 # Add your code here
-def run(songs)
-  while true do
-    puts "Please enter a command:"
-    response = gets.chomp
-    case response
-    when "exit"
-      exit_jukebox
-      break
-    when "play"
-      play(songs)
-    when "help"
-      help
-    when "list"
-      list(songs)
-    else
-      puts "Invalid entry"
-    end
-  end
-end
+
 
 def play(songs)
   puts "Please enter a song name or number:"
@@ -47,4 +29,24 @@ def list(songs)
   songs.each_with_index {|song,index|
     puts "#{index+1}. #{song}"
   }
+end
+
+def run(songs)
+  while true do
+    puts "Please enter a command:"
+    response = gets.chomp
+    case response
+    when "exit"
+      exit_jukebox
+      break
+    when "play"
+      play(songs)
+    when "help"
+      help
+    when "list"
+      list(songs)
+    else
+      puts "Invalid entry"
+    end
+  end
 end
