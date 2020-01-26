7c7100012bd9255513b182e4bca7e2cd66dcb04c
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-26 01:55:06 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,3 +30,21 @@ def play(songs)
     puts "Invalid input, please try again"
   end
 end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def help
+  puts "I accept the following commands"
+  puts  "- help : displays this help message"
+  puts  "- list : displays a list of songs you can play"
+  puts  "- play : lets you choose a song to play"
+  puts  "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index {|song,index|
+    puts "#{index+1}. #{song}"
+  }
+end
